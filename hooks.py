faaffd1703a603e3e53c7494c34ff522ce3f9a16
pubmania <mr.ankitmittal@gmail.com>
2024-11-01 16:19:19 +0000

    Relative Image Path for recipes

--- hooks.py
+++ hooks.py
@@ -4,10 +4,28 @@ import markdown
 from parser.parse_recipe import fn_extract_cooklang_blocks, fn_parse_recipe
 
 def on_page_markdown(markdown, page, **kwargs):
+    current_page_path = page.file.src_path
+    current_dir = os.path.dirname(current_page_path)
+     # Define the base image path
+    base_image_path = 'assets/images/'
+    # Check if the current directory is within the Recipes directory
+    recipes_dir = os.path.abspath('Recipes')  # Get the absolute path of the Recipes directory
+    current_dir_abs = os.path.abspath(current_dir)  # Get the absolute path of the current directory
+
+    # Determine the relative path to the images
+    if os.path.commonpath([recipes_dir, current_dir_abs]) == recipes_dir:
+        # If current_dir is a subdirectory of Recipes, calculate how many levels to go up
+        relative_path_for_image = os.path.relpath(base_image_path, current_dir_abs)
+    else:
+        # Default to the base image path if not in Recipes
+        relative_path_for_image = base_image_path
+
+    print(relative_path_for_image)
+
     cooklang_content = fn_extract_cooklang_blocks(markdown)
     if cooklang_content:
         for content in cooklang_content:
-            processed_output = fn_parse_recipe(content.strip())
+            processed_output = fn_parse_recipe(content.strip(),relative_path_for_image)
             #replace codeblock in markdown with processd_output in the content
             markdown = markdown.replace(f"```cooklang\n{content}\n```",processed_output)
         #print(f"------------------\n{md_content}\n-------------------")
