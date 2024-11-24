676f262e8bb60012650185be4ff6431ecdca8420
pubmania <mr.ankitmittal@gmail.com>
2024-11-24 22:25:34 +0000

    UI changes for better dsiplay on small screen size (#43)
    * Update nutrition_labels_creator.py
    
    table adjustments for mobile screen
    
    * Update header.html
    
    remove overlap of Previous and Next page arrows on mobile screen
    
    * Update extra.css
    
    remove extra padding in table column width.
    
    * Update colors.html
    
    changed few colours which were too dark for dark mode.

--- parser/nutrition_labels_creator.py
+++ parser/nutrition_labels_creator.py
@@ -177,5 +177,5 @@ def fn_total_df_weight(input_string) -> tuple[dict, str, int]:
     }, inplace=True)
     #net_carbs_table_found_ingredients = found_entries[['Ingredient', 'Quantity', 'Unit', 'Qty in gms', 'Source', 'Net Carbs (gms)']].round(2).to_markdown(index=False)
     #nutrient_labels = f"## Nutrition Label\n\n{get_label_string(total_weight_df,missing_ingredients_string,serving_size)}"
-    net_carbs_table_found_ingredients = found_entries[['Ingredient', 'Qty in recipe', 'Qty in gms', 'Net Carbs (gms)', 'Source']].round(2).to_markdown(index=False)
+    net_carbs_table_found_ingredients = found_entries[['Ingredient', 'Qty in recipe', 'Net Carbs (gms)', 'Qty in gms', 'Source']].round(2).to_markdown(index=False)
     return total_weight_df.to_dict(), missing_ingredients_string, serving_size, net_carbs_table_found_ingredients
